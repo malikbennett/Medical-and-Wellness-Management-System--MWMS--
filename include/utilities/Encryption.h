4c530b031836a4194ba4dccbc43c00ec6c5fd30f
Malik Bennett <bennettmalik312@gmail.com>
2025-04-05 05:43:11 -0500

    Created Encryption Methods
    - Using XOR Encryption Technique
    - Added Method to get key form encryption file
    - Store output as HEX FORMAT

--- include/utilities/Encryption.h
+++ include/utilities/Encryption.h
@@ -1,6 +1,8 @@
 #pragma once
 
-#include <string>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
 
 using namespace std;
 
@@ -9,18 +11,51 @@ class Encrypt
 {
 public:
     // Returns an instance of Encrypt Class which belongs to the class itself
-    static Encrypt& getInstance(){
+    static Encrypt &getInstance()
+    {
         static Encrypt instance;
         return instance;
     }
-    // Encrypt or Decrypy a text using a key with the XOR operator
-    void encryptDecrypt(string &text, char key)
+
+    // Read the key from a file
+    void readKeyFromFile(const string &filePath, string &key)
     {
-        for (char &c : text)
+        try
         {
-            c ^= key;
+            ifstream keyFile(filePath);
+            if (!keyFile)
+            {
+                throw runtime_error("Error: Could not open the key file.");
+            }
+            getline(keyFile, key);
+        }
+        catch (const std::exception &e)
+        {
+            std::cerr << e.what() << '\n';
         }
     }
+    // Returns Encrypted input using XOR Encryption | Returns In HEX Format
+    string xorEncryptDecrypt(const string &input, const string &key)
+    {
+        string output = input;
+
+        for (size_t i = 0; i < input.size(); ++i)
+        {
+            output[i] ^= key[i % key.size()]; // Loop through the key
+        }
+        return toHex(output);
+    }
+    // Converts input to HEX format | Returns HEX Format
+    string toHex(const string &input)
+    {
+        ostringstream oss;
+        for (unsigned char c : input)
+        {
+            oss << hex << setw(2) << setfill('0') << (int)c;
+        }
+        return oss.str();
+    }
+
 private:
     Encrypt() {};
 };
