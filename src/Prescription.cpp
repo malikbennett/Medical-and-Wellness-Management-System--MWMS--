189af163d176fd49114fcf3db899171265586f2d
LocalAngel <alexiasimpson6@gmail.com>
2025-04-15 22:39:50 -0500

    Add file storage and retrieval functions to Prescription class. Closes #32

--- src/Prescription.cpp
+++ src/Prescription.cpp
@@ -1,11 +1,46 @@
+
 #include "core/Prescription.h"
+#include <fstream>     
+#include <sstream>     
+#include <iostream>    
+
 
 void Prescription::approve() {
     isApproved = true;
 }
+
 
 bool Prescription::getApprovalStatus() const {
     return isApproved;
 }
 
 
+void Prescription::saveToFile(std::ofstream& out) const {
+    out << prescriptionNumber << ','
+        << patientNumber << ','
+        << doctorNumber << ','
+        << medication << ','
+        << dosage << ','
+        << prescriptionDate.day << '/'
+        << prescriptionDate.month << '/'
+        << prescriptionDate.year << ','
+        << isApproved << '\n';
+}
+
+
+void Prescription::loadFromFile(std::ifstream& in) {
+    char comma, slash;
+    std::string med;
+    std::getline(in, med, ','); 
+
+    in >> prescriptionNumber >> comma
+       >> patientNumber >> comma
+       >> doctorNumber >> comma;
+
+    std::getline(in, medication, ',');
+    in >> dosage >> comma
+       >> prescriptionDate.day >> slash
+       >> prescriptionDate.month >> slash
+       >> prescriptionDate.year >> comma
+       >> isApproved;
+}
